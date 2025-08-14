90a932faed7b88ba7ae845f2aadf97ba6ae26c17
Sena Çetinkaya <67286519+sena-cetinkaya@users.noreply.github.com>
2025-08-14 13:57:37 +0300

    upload_to_database.py

--- scripts/upload_to_database.py
+++ scripts/upload_to_database.py
@@ -2,17 +2,11 @@ from sqlmodel import SQLModel, Session, create_engine
 import pandas as pd
 from app.schemas import News
 
-# Veritabanı bağlantısı URL'sini direkt yaz
 engine = create_engine(r"postgresql://postgres:postgres@localhost:5437/news_dev")
 #engine = create_engine(r"postgresql://postgres:postgres@localhost:5438/news_test")
 
-# CSV dosyasını oku
 df = pd.read_csv("../data/cleaned_data.csv")
 
-# Tablo oluştur
-#SQLModel.metadata.create_all(engine)
-
-# Verileri ekle
 with Session(engine) as session:
     for _, row in df.iterrows():
         news_item = News(news=row["clean_text"], result=row["Sonuc"])
@@ -20,5 +14,6 @@ with Session(engine) as session:
         session.commit()
 
 print("Veriler başarıyla yüklendi.")
+
 
 
